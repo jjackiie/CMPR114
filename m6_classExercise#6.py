bb6c714a8a81f29877ded93bb4f6ba2e587d4657
Jacqueline C <79559850+jjackiie@users.noreply.github.com>
2023-03-20 19:14:20 -0700

    Class Exercise 6

--- m6_classExercise#6.py
+++ m6_classExercise#6.py
@@ -8,7 +8,7 @@
 # Programmer: Jacqueline Ceballos
 # Class: CMPR 114
 # ====================================
-
+'''
 def write():
     print("----Enter your information----")
 
@@ -41,6 +41,7 @@ def read():
 
 
 read()
+'''
 ''''
 =================== Output ===========================
 ----Enter your information----
@@ -53,6 +54,75 @@ Info recorded!
 Jacqueline
 Ceballos
 21
+
+
+Process finished with exit code 0
+
+'''
+
+
+# ====================================
+# Attached: Class Exercise #6
+# ====================================
+# File: Challenge #2
+# ====================================
+# Name: Write Numbers
+# ====================================
+# Programmer: Jacqueline Ceballos
+# Class: CMPR 114
+# ====================================
+
+def writeNumbers():
+    outfile = open("m6_numbers.txt", "a")
+
+    num1 = int(input("Enter #1: "))
+    num2 = int(input("Enter #2: "))
+    num3 = int(input("Enter #3: "))
+
+    sum = num1 + num2 + num3
+    average = sum / 3
+
+    outfile.write("The 1st number is " + str(num1) + "\n")
+    outfile.write("The 2nd number is " + str(num2) + "\n")
+    outfile.write("The 3rd number is " + str(num3) + "\n")
+    outfile.write("\nThe average number is " + str(average) + "\n")
+    outfile.write("\nThe total number is " + str(sum) + "\n")
+
+    outfile.close()
+
+    print("\nDate Recorded!")
+
+
+writeNumbers()
+
+
+def read():
+    infile = open("m6_numbers.txt", "r")
+
+    fileContents = infile.read()
+
+    infile.close()
+
+    print(fileContents)
+
+
+read()
+
+''''
+=================== Output ===========================
+Enter #1: 22
+Enter #2: 33
+Enter #3: 44
+
+Date Recorded!
+
+The 1st number is 22
+The 2nd number is 33
+The 3rd number is 44
+
+The average number is 33.0
+
+The total number is 99
 
 
 Process finished with exit code 0
