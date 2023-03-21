ad95064f37b4ccf083a62b81d2ed2ee7c11d2f36
Jacqueline C <79559850+jjackiie@users.noreply.github.com>
2023-03-20 19:41:35 -0700

    Class Exercise 6

--- m6_classExercise#6.py
+++ m6_classExercise#6.py
@@ -60,7 +60,6 @@ Process finished with exit code 0
 
 '''
 
-
 # ====================================
 # Attached: Class Exercise #6
 # ====================================
@@ -71,10 +70,10 @@ Process finished with exit code 0
 # Programmer: Jacqueline Ceballos
 # Class: CMPR 114
 # ====================================
-
+'''
 def writeNumbers():
     outfile = open("m6_numbers.txt", "a")
-
+    
     num1 = int(input("Enter #1: "))
     num2 = int(input("Enter #2: "))
     num3 = int(input("Enter #3: "))
@@ -107,7 +106,7 @@ def read():
 
 
 read()
-
+'''
 ''''
 =================== Output ===========================
 Enter #1: 22
@@ -126,5 +125,74 @@ The total number is 99
 
 
 Process finished with exit code 0
+
+'''
+
+
+# ====================================
+# Attached: Class Exercise #6
+# ====================================
+# File: Challenge #3
+# ====================================
+# Name: Sales Data
+# ====================================
+# Programmer: Jacqueline Ceballos
+# Class: CMPR 114
+# ====================================
+
+def sales():
+    num_days = int(input("Enter the days of sales: "))
+    sales_files = open("m6_sales.txt", "a")
+
+    for count in range(1, num_days + 1):
+        sales = float(input("Enter the sales for Day #" + str(count) + ": "))
+        sales_files.write(str(sales) + "\n")
+    sales_files.close()
+
+    print("Sales Recorded!")
+
+    salary = int(input("\nEnter your salary: "))
+    if salary <= 1000:
+        print(salary)
+    elif salary > 1000:
+        subtotal = (salary * .10)
+        total = subtotal + salary
+        print("The total is " + str(total))
+
+
+sales()
+
+
+def readSales():
+    sales_file = open("m6_sales.txt", "r")
+
+    line = sales_file.readline()
+
+    while line != " ":
+        amount = float(line)
+
+        print(format(amount, ".2f"))
+
+        line = sales_file.readline()
+
+    sales_file.close()
+
+
+readSales()
+
+''''
+=================== Output ===========================
+Enter the days of sales: 5
+Enter the sales for Day #1: 1000
+Enter the sales for Day #2: 1000
+Enter the sales for Day #3: 1000
+Enter the sales for Day #4: 1000
+Enter the sales for Day #5: 1000
+Sales Recorded!
+
+Enter your salary: 80000
+The total is 88000.0
+
+Process finished with exit code 1
 
 '''
